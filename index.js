ec40441e07e57d2758bd97f78675280116c2e223
george <georgge.golder@student.moringaschool.com>
2024-10-21 22:41:54 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Antony", "Beverley", "Faith", "Bob", "George", "Dalton"];
+const messages = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+
+countDown(10);
